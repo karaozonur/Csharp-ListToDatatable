e0b3d0f3c968a681fd3510dcde60e1487755e8e0
karaoz.onr@gmail.com <karaoz.onr@gmail.com>
2023-08-29 20:38:32 +0300

    Add project files.

---
+++ CsharpListToDatatable/ListToDatatableConvert.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpListToDatatable
+{
+    public class ListToDatatableConvert
+    {
+        public DataTable ToDataTable<T>(List<T> items)
+        {
+            DataTable dt = new DataTable(typeof(T).Name);
+            PropertyInfo[] properties = typeof(T).GetProperties(BindingFlags.Public|BindingFlags.Instance);
+            foreach (PropertyInfo property in properties)
+            {
+                dt.Columns.Add(property.Name);
+            }
+            foreach (T item in items)
+            {
+                var values = new object[properties.Length];
+                for(int i=0;i< properties.Length;i++)
+                {
+                    values[i] = properties[i].GetValue(item,null);   
+                }
+                dt.Rows.Add(values);
+            }
+            return dt;
+        } 
+    }
+}
---
+++ CsharpListToDatatable/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpListToDatatable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> list = new List<Student>() { 
+            new Student() { Name = "Onur", Id = 1, Department = "Bilişim" },
+            new Student() { Name = "Abone Ol", Id = 2, Department = "beğen" }
+            };
+
+            ListToDatatableConvert convert = new ListToDatatableConvert();  
+            DataTable dataTable = convert.ToDataTable(list);
+
+          foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (DataColumn col  in dataTable.Columns)
+                {
+                    Console.WriteLine(row["Name"].ToString());
+                }
+            }
+          
+            Console.ReadKey();
+        }
+    }
+}
